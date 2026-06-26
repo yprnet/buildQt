2204a719937ad37de36692f0a5ee8d7a5b8642f4
YuanPeirong <yuanpeirong@vip.qq.com>
2026-06-26 22:30:44 +0800

    完整版本：增加参数 -feature-linguist -qt-tools -make libs -make tools
    参数解释：
    -feature-linguist
    开启 linguist、lupdate、lrelease 翻译工具编译，没有这条，LinguistTools 模块直接不编译。
    
    -qt-tools + -make tools
    编译 Qt 自带工具集，会自动生成 LinguistTools 对应的整套 CMake 脚本，包括缺失的：
    Qt6LinguistToolsAdditionalTargetInfo.cmake
    
    当前文件缺失的根本原因：只编译了libs，没编译 tools。

--- buildQt6/build-Qt6llvm-mingw17.0.6.cmd
+++ buildQt6/build-Qt6llvm-mingw17.0.6.cmd
@@ -30,7 +30,7 @@ rmdir /s /q "%BUILD_DIR%"
 mkdir "%BUILD_DIR%" && cd /d "%BUILD_DIR%"
 
 :: configure
-call %SRC_QT%\configure.bat -static -static-runtime -release -prefix %INSTALL_DIR% -nomake examples -nomake tests -skip qtwebengine -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel
+call %SRC_QT%\configure.bat -static -static-runtime -release -prefix %INSTALL_DIR% -nomake examples -nomake tests -skip qtwebengine -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -feature-linguist -qt-tools -make libs -make tools
 
 :: 编译(不要忘记点)
 cmake --build . --parallel
