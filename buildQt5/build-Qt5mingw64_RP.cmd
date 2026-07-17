13ab54e3ab955b6fc078826e3cc5c6e830aded62
YuanPeirong <yuanpeirong@vip.qq.com>
2026-07-17 14:11:40 +0800

    Qt5源码修复：修改补丁文件位置

--- buildQt5/build-Qt5mingw64_RP.cmd
+++ buildQt5/build-Qt5mingw64_RP.cmd
@@ -25,8 +25,8 @@ SET SRC_qttranslations="%QT_PATH%\%QT_VERSION%\qttranslations-everywhere-src-%QT
 SET SRC_qtsvg="%QT_PATH%\%QT_VERSION%\qtsvg-everywhere-src-%QT_VERSION%"
 
 ::替换qfilesystemengine_win.cpp、main.c(使其可以被高于MinGW GCC8.1.0版本编译)
-copy %~dp0\qfilesystemengine_win.cpp %SRC_qtbase%\src\corelib\io\qfilesystemengine_win.cpp /Y
-copy %~dp0\main.c %SRC_qttools%\src\assistant\qcollectiongenerator\main.c /Y
+copy %~dp0\patches\qfilesystemengine_win.cpp %SRC_qtbase%\src\corelib\io\qfilesystemengine_win.cpp /Y
+copy %~dp0\patches\main.c %SRC_qttools%\src\assistant\qcollectiongenerator\main.c /Y
 
 :: 设置安装文件夹目录
 SET INSTALL_DIR="%QT_PATH%\%QT_VERSION%-static\%MinGW_VERSION%"
